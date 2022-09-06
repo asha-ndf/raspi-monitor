79d42ba61b1a35bb15f3e164c07c3c833c535d8a
asha-ndf <ashanomail@gmail.com>
2022-09-06 19:52:46 +0900

    csvを追記に変更

--- raspi/main.py
+++ raspi/main.py
@@ -11,7 +11,7 @@ for i in range(10):
     time.sleep(1)
     
 print(data)
-with open(r'./data/data.csv','w',encoding='utf-8', newline="") as csvfile:
+with open(r'./data/data.csv','a',encoding='utf-8', newline="") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames = field_name)
     writer.writeheader()
     writer.writerows(data)
